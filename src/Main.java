c059300ed42324ea21a21d15122cb9a737bb7c28
Henriette Lillehagen <henriettelillehagen@gmail.com>
2019-09-24 08:39:00 +0200

    git endringer av henriette

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String myUsername = "this is my username";
         String myPassword = "This is my password";
+        String test = "Hei";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
